3fb9cf550b73efa8845017b0ae8fe06423e03e34
Cedric PAILLE <cedric@bofur.dwarf.local>
2016-09-16 14:06:35 +0200

    Added scroll widget + drag fixes

---
+++ ui/scroll.cpp
@@ -0,0 +1,63 @@
+#include "scroll.h"
+
+Scroll::Scroll(int x, int y, int width, int height, const char* name, Widget* parent) : Widget(x, y, width, height, name, parent)
+{
+	m_scroll_widget = NULL;
+	m_oldx = m_oldy = 0;
+}
+
+bool
+Scroll::drag_event(int rel_x, int rel_y)
+{
+	if (!m_scroll_widget)
+		return false;
+
+	int new_pos_x = m_oldx - rel_x;
+	int new_pos_y = m_oldy - rel_y;
+	int scroll_widget_w = m_scroll_widget->w();
+	int scroll_widget_h = m_scroll_widget->h();
+
+	if (new_pos_x < -(scroll_widget_w - w()))
+		new_pos_x = -(scroll_widget_w - w());
+
+	if (new_pos_y < -(scroll_widget_h - h()))
+		new_pos_y = -(scroll_widget_h - h());
+
+	new_pos_x = new_pos_x > 0 ? 0 : new_pos_x;
+	new_pos_y = new_pos_y > 0 ? 0 : new_pos_y;
+
+	m_scroll_widget->x(new_pos_x);
+	m_scroll_widget->y(new_pos_y);
+
+	dirty(true);
+	return true;
+}
+
+void
+Scroll::set_scroll_widget(Widget* scroll_widget)
+{
+	if (!scroll_widget){
+		std::cerr << "Scroll::set_scroll_widget : inserting NULL widget" << std::endl;
+		return;
+	}
+	m_scroll_widget = scroll_widget;
+	if (m_scroll_widget->parent() != this){
+		m_scroll_widget->parent(this);
+	}
+	m_scroll_widget->x(0);
+	m_scroll_widget->y(0);
+}
+
+bool
+Scroll::accept_drag()
+{
+	m_oldx = m_scroll_widget->x();
+	m_oldy = m_scroll_widget->y();
+	return true;
+}
+
+void
+Scroll::draw()
+{
+
+}
---
+++ ui/scroll.h
@@ -0,0 +1,20 @@
+#ifndef SCROLL_H
+#define SCROLL_H
+
+#include "widget.h"
+
+class Scroll : public Widget
+{
+	Widget* m_scroll_widget;
+	int m_oldx, m_oldy;
+protected:
+	virtual void draw();
+public:
+	Scroll(int x, int y, int width, int height, const char* name = "", Widget* parent = NULL);
+	virtual bool accept_drag();
+	virtual bool drag_event(int rel_x, int rel_y);
+
+	void set_scroll_widget(Widget* scroll_widget);
+};
+
+#endif
